6f0d049ec654d4281afe388566f570e280f8928b
Anurag Bhardwaj <anurag.wizard@gmail.com>
2015-06-11 15:22:31 -0700

    comments addition

--- index.js
+++ index.js
@@ -15,22 +15,29 @@ if (!region || region === null || region === "") {
 	console.log("AWS Lambda Redshift Database Loader using default region " + region);
 }
 
+//Requiring aws-sdk. 
 var aws = require('aws-sdk');
 aws.config.update({
 	region : region
 });
+
+//Requiring S3 module. 
 var s3 = new aws.S3({
 	apiVersion : '2006-03-01',
 	region : region
 });
+//Requiring dynamoDB module. 
 var dynamoDB = new aws.DynamoDB({
 	apiVersion : '2012-08-10',
 	region : region
 });
+
+//Requiring SNS module. 
 var sns = new aws.SNS({
 	apiVersion : '2010-03-31',
 	region : region
 });
+
 require('./constants');
 var kmsCrypto = require('./kmsCrypto');
 kmsCrypto.setRegion(region);
@@ -41,14 +48,18 @@ var pg = require('pg');
 var upgrade = require('./upgrades');
 var zlib = require('zlib');
 
+//Connection string to connect to Redshift with username and password. 
 var conString = "postgresql://abhardwaj:Master12@rs-instance.cysomezynckr.us-west-2.redshift.amazonaws.com:5439/mydb";
+
+//Query string to insert data into Redshift. 
 var queryTextInsert = 'INSERT INTO suned_redshift (suned_cust_id, quote_system_size, quote_ef_cost_per_watt, quote_year1_production, cust_pre_payment, quote_master_lease_pay_esc_rate, quote_rebate, quote_hipbi_year1_value, quote_hipbi_tenure, quote_hipbi_annual_derate, quote_state_tax_rate, quote_current_utility_cost, quote_post_solar_utility_cost, quote_proposal_id, quote_call_version_id, quote_auth_code, system_module_id, system_module_quantity, system_inverter_id, system_inverter_quantity, system_mounting_type, contract_calcmap_current_date, contract_installer_client_name, contract_calcmap_dealer_name, contract_calcmap_howner_0_first_name, contract_calcmap_howner_0_last_name, contract_calcmap_howner_1_first_name, contract_calcmap_howner_1_last_name, contract_product_type, contract_calcmap_n_of_howners, contract_calcmap_howner_0_address, contract_calcmap_howner_0_city, contract_calcmap_howner_0_state, contract_calcmap_howner_0_zipcode, contract_calcmap_howner_0_phone, contract_calcmap_howner_0_email, contract_calcmap_howner_1_address, contract_calcmap_howner_1_city, contract_calcmap_howner_1_state, contract_calcmap_howner_1_zipcode, contract_calcmap_howner_1_phone, contract_calcmap_howner_1_email, contract_calcmap_howner_2_address, contract_calcmap_howner_2_city, contract_calcmap_howner_2_state, contract_calcmap_howner_2_zipcode, contract_calcmap_howner_2_phone, contract_calcmap_howner_2_email, contract_installer_client_phone, contract_production_0_col2, contract_calcmap_lifetime_kwh, array_number, module_type, module_quantity, shading, tilt, azimuth, orientation, monthly_production_values, degradation_rates) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39,$40,$41,$42,$43,$44,$45,$46,$47,$48,$49,$50,$51,$52,$53,$54,$55,$56,$57,$58,$59,$60)';
+
+//Query string to fetch data from Redshift. 
 var queryFetch = 'SELECT * from suned_redshift where suned_cust_id = $1 order by array_number asc';
 
-// main function for AWS Lambda
-
+// Main function for AWS Lambda
 exports.handler = function(event, context) {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
+
     // Get the object from the event and show its content type
     var bucket = event.Records[0].s3.bucket.name;
     var key = event.Records[0].s3.object.key;
@@ -65,8 +76,10 @@ exports.handler = function(event, context) {
         }
     });
 
+    //Method to insert data into Redshift. 
 	var insertData = function(inbound_payload){
 
+		//Local variables definition to get data from JSON file. 
 		var SunEdCustId = inbound_payload.SunEdCustId;
         var SystemSize = inbound_payload.Quote.SystemSize;
         var EFCostPerWatt = inbound_payload.Quote.EFCostPerWatt;
@@ -125,11 +138,15 @@ exports.handler = function(event, context) {
         var lifeTimeKwh = inbound_payload.Contract["calcMap.lifetimekWh"];	
 
 		var suned_id = parseInt(SunEdCustId, 10);
+
+		//Establishing connection to Redshift using postgres. 
 		pg.connect(conString, function(err,client){
 			if(err){
 				return console.log("Connection error. ", err);
 			}
 			console.log("Connection Established under fetch");
+
+			//Querying redshift. 
 			client.query(queryFetch, [SunEdCustId], function(err,result){
 				if(err){
 					console.log("Error returning query", err);
@@ -137,13 +154,17 @@ exports.handler = function(event, context) {
 				}
 				console.log("Number of rows: ", result.rows.length);
 				console.log("Number of rows from JSON" + inbound_payload.Array.length);
+
+				//Algorithm to check redundancy and add unique data into redshift. 
 				for(var m=0;m<inbound_payload.Array.length;m++){
                   	
+                  	//Insert all the data from JSON file if no data exists in Redshift. 
                   	if(result.rows.length == 0){
                         console.log("No records in Redshift");
                         insertIntoRedshift(m);
                   	}
 
+                  	//Check for duplicacy and insert rows to redshift. 
                   	else{
                   		for(var k=0;k<result.rows.length;k++){
 	                    	if(result.rows[k].suned_cust_id == SunEdCustId && result.rows[k].array_number == inbound_payload.Array[m].ArrayNumber){
@@ -159,6 +180,7 @@ exports.handler = function(event, context) {
 			});
 		});
 
+		//Method to run insert query to push data into redshift. 
 		var insertIntoRedshift = function(m){
 			pg.connect(conString, function(err,client){
         		if(err){
